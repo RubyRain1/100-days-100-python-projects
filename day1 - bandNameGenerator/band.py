defe3824ca1850eacb468e98bb82726ae7b6f861
RubyRain <100702336+RubyRain1@users.noreply.github.com>
2024-05-08 16:14:26 -0400

    day one project
    this is the program for the first day of the 100 day coding course. It is a band name generator utilizing print and input statements.

---
+++ day1 - bandNameGenerator/band.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+name = input("What is the name of the city you greq up in? \n")
+petname = input("what is your pet's name? \n")
+
+print("Your band name could be " + name + petname)
+
