219f695fb2f88b4a47372011562a42106ec9b452
RubyRain <100702336+RubyRain1@users.noreply.github.com>
2024-05-08 21:14:48 -0400

    day two tip calculator MY SOLUTION
    this is my solution to the day 2 project, it takes the total bill, calculates for percent tip then divides by the number of people. to display this information it utilizes an f  string.

--- day2- tipCalculator/tip.py
+++ day2- tipCalculator/tip.py
@@ -1,1 +1,9 @@
-print("hello")
+print("Welcome to the tip calculator!")
+
+total = float(input("what was the total bill? $"))
+perctip = float(input("how much tip would you like to give? 10, 12, or 15? "))
+people = int(input("how many people to split the bill? "))
+
+tip = round((total / people) * ((perctip/100)+1), 2)
+
+print(f"each person should pay: ${tip}") 
