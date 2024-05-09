2bb56c72ea8d62086aee1a7bd2b91dbd884d9eb6
RubyRain <100702336+RubyRain1@users.noreply.github.com>
2024-05-08 21:19:43 -0400

    day 2 formatting
    this allows for better float formatting

--- day2- tipCalculator/tip.py
+++ day2- tipCalculator/tip.py
@@ -5,5 +5,5 @@ perctip = float(input("how much tip would you like to give? 10, 12, or 15? "))
 people = int(input("how many people to split the bill? "))
 
 tip = round((total / people) * ((perctip/100)+1), 2)
-
+tip = "{:.2f}".format(tip)
 print(f"each person should pay: ${tip}") 
