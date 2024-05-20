d60c67622bd9307304beb6e4db6bcec51bc80a92
Ruby Rain <nhhughes10@gmail.com>
2024-05-20 13:42:36 -0400

    UI update
    this is adding a good ascii UI

--- day7- hangman/hang_man.py
+++ day7- hangman/hang_man.py
@@ -1,15 +1,77 @@
 import random
 
-wList = ["ardvark", "baboon", "camel"]
+wList = ["ardvark", "baboon", "camel", "zebra"]
 chosenlist = []
 wrong = 0 
 check = "_"
+
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+
+title = '''
+  _                                             
+ | |                                            
+ | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ | | | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                     __/ |                      
+                    |___/                       
+                    '''
 #this will choose a random word based off index number
-rnum = random.randint(0,2)
+rnum = random.randint(0,3)
 
 #assigns the random number in order to get the string instead of index number
 chosenword = wList[rnum]
-print(chosenword)
 
 #this populates a list full of _'s to act as the blank template.
 for i in range(0,len(chosenword)):
--- day7- hangman/solution.py
+++ day7- hangman/solution.py
@@ -1,8 +1,15 @@
 import hang_man
 
+
+
+
+
+
 def game():
 
-    while hang_man.check in hang_man.chosenlist and hang_man.wrong <= 7:
+    print(hang_man.title)   
+    
+    while hang_man.check in hang_man.chosenlist and hang_man.wrong <= 5:
 
         guess = input("guess a letter: ").lower()
 
@@ -13,27 +20,29 @@ def game():
         if guess not in hang_man.chosenword and hang_man.wrong == 0:
             print("not in word")
             hang_man.wrong +=1
+            print(hang_man.HANGMANPICS[1])
         elif guess not in hang_man.chosenword and hang_man.wrong == 1:
             print("not in word")
             hang_man.wrong +=1
+            print(hang_man.HANGMANPICS[2])
         elif guess not in hang_man.chosenword and hang_man.wrong == 2:
             print("not in word")
             hang_man.wrong +=1
+            print(hang_man.HANGMANPICS[3])
         elif guess not in hang_man.chosenword and hang_man.wrong == 3:
             print("not in word")
             hang_man.wrong +=1
+            print(hang_man.HANGMANPICS[4])
         elif guess not in hang_man.chosenword and hang_man.wrong == 4:
             print("not in word")
             hang_man.wrong +=1
+            print(hang_man.HANGMANPICS[5])
         elif guess not in hang_man.chosenword and hang_man.wrong == 5:
             print("not in word")
             hang_man.wrong +=1
-        elif guess not in hang_man.chosenword and hang_man.wrong == 6:
-            print("not in word")
-            hang_man.wrong +=1
-        elif guess not in hang_man.chosenword and hang_man.wrong == 7:
-            hang_man.wrong +=1
-            print(f"GAME OVER {hang_man.chosenword} WAS THE CORRECT WORD")
+            print(hang_man.HANGMANPICS[6])
+            print(f"GAME OVER {hang_man.chosenword.upper()} WAS THE CORRECT WORD")
+            
                             
 
         #checks if a letter is in the chosen word and then
@@ -41,10 +50,11 @@ def game():
         for i in range(0,len(hang_man.chosenword)):
             if guess in hang_man.chosenword[i]:
                 hang_man.chosenlist[i] = guess
+                print(hang_man.HANGMANPICS[hang_man.wrong])
             
 
 
-        if hang_man.wrong <= 7:
+        if hang_man.wrong <= 5:
             result = ' '.join(hang_man.chosenlist)
             print(result)
 
