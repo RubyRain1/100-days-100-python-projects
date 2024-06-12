c7b6d0d3b5307b8b542e13c373e535ce41eabd80
Ruby Rain <nhhughes10@gmail.com>
2024-06-12 14:34:56 -0400

    tread

--- day9 - SecretAuction/auction.py
+++ day9 - SecretAuction/auction.py
@@ -2,25 +2,15 @@ print(''' this will be ascii art soon ''')
 print("Welcome to the secret auction program.")
 
 
- 
+bids = {}
 cont = True
 
 while cont:
 #this is the start input 
     name = input("what is your name?: ")
     bid = input("what is your bid?: ")
-    
-    auctionL = [{"name": name, "bid": bid,}]
-    
-
-    #function to add nested dictionaries into a list to distinguish between auctioneers. 
-    
-    def auction(name, bid):
-        auctiondict = {
-            "name": name,
-            "bid": bid
-            }
-        auctionL.append(auctiondict);
+    bids[name] = bid
+        
     userI = input("are there any other bidders? 'yes' or 'no'. ").lower()
 
     if userI == 'yes':
@@ -29,5 +19,4 @@ while cont:
     elif userI == 'no':
         cont = False
         
-auction(name, bid)
-print(auctionL)
+
