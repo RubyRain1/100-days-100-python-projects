2d478e923089a0eb2c82dd517443dfa4b987d261
Ruby Rain <nhhughes10@gmail.com>
2024-07-15 15:00:31 -0400

    OOP practice
    OOP practice

--- day16 - OOP1/main.py
+++ day16 - OOP1/main.py
@@ -1,6 +1,26 @@
-from turtle import Turtle
+# from turtle import Turtle, Screen
+#
+# timmy =Turtle()
+# count = 0
+#
+# def square():
+#     timmy.forward(100)
+#     timmy.left(90)
+#     timmy.forward(100)
+#
+# timmy.shape("turtle")
+# timmy.color("chartreuse")
+#
+#
+# myScreen = Screen()
+# myScreen.bgcolor("black")
+#
+# while count < 5:
+#     square()
+#     count+= 1
+#
+#
+# myScreen.exitonclick()
 
+import prettytable
 
-timmy =Turtle()
-
-print(timmy)
