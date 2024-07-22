5653120e8ef9a595939ea2c8422bfe56ec129b34
Holden Hughes <100702336+RubyRain1@users.noreply.github.com>
2024-07-22 11:00:44 -0400

    random color update
    this changes the random color

--- day18- turtle painting/turt.py
+++ day18- turtle painting/turt.py
@@ -1,21 +1,20 @@
-import turtle as t #this assigns turtle to t to save time 
+import turtle as t  # this assigns turtle to t to save time
 from turtle import Turtle, Screen
 import random
 
-
 timmy = Turtle()
 
-color_table = ['red','blue','green','orange','pink','white','cyan','chartreuse']
-
+color_table = ['red', 'blue', 'green', 'orange', 'pink', 'white', 'cyan', 'chartreuse']
 
 timmy.shape("turtle")
 timmy.color("chartreuse")
 
+
 def drawshape(num_sides):
     angle = 360 / num_sides
+    color_random = random.randint(0, 7)
+    timmy.pencolor(color_table[color_random])
     for i in range(num_sides):
-        color_random = random.randint(0,7)
-        timmy.pencolor(color_table[color_random])
         timmy.forward(100)
         timmy.right(angle)
 
@@ -25,8 +24,7 @@ myScreen.bgcolor("black")
 timmy.penup()
 timmy.setpos(0, 200)
 timmy.pendown()
-for sides in range(3,11):
+for sides in range(3, 11):
     drawshape(sides)
-
 
 myScreen.exitonclick()
