b72a548752e49ffbb9a1097e4e34005219480f89
Ruby Rain <nhhughes10@gmail.com>
2024-08-01 14:24:23 -0400

    finsih
    this is the finished project

--- day18- turtle painting/painting.py
+++ day18- turtle painting/painting.py
@@ -18,46 +18,57 @@ color_list = [(238, 244, 250), (236, 224, 81), (197, 7, 70), (195, 164, 14), (11
               (211, 136, 177), (232, 223, 7), (199, 33, 132), (13, 183, 212), (230, 167, 199), (126, 189, 161),
               (9, 49, 28), (40, 132, 76), (128, 219, 232), (61, 12, 25), (67, 22, 7), (112, 90, 210), (147, 215, 199),
               (178, 17, 8), (234, 66, 34)]
+print(len(color_list))
 count = 0
 cont = True
+rows = 0
+y = 0
 t.colormode(255)  #allows the use of 0,255 rgb
 
 def dotcolor():
-    rColor = color_list[random.randint(0, len(color_list) - 1)]
+    rColor = color_list[random.randint(0, 26)]
     return rColor
 
 def dotrow():
     global turn_logic, turn_check
-    ruby_jr.speed("fastest")
-    ruby_jr.dot(20, dotcolor())
-    for i in range(0,10):
-        ruby_jr.penup()
-        ruby_jr.forward(5)
-        ruby_jr.pendown()
+    timmy.speed("fastest")
+    timmy.dot(40, dotcolor())
+    for i in range(0,11):
+        timmy.penup()
+        timmy.forward(5)
+        timmy.pendown()
 
 
 def turnleft():
-    ruby_jr.penup()
-    ruby_jr.left(90)
-    ruby_jr.forward(20)
-    ruby_jr.left(90)
+    timmy.speed("fastest")
+    timmy.penup()
+    timmy.left(90)
+    timmy.forward(20)
+    timmy.left(90)
 
-def turnright():
-    ruby_jr.penup()
-    ruby_jr.right(90)
-    ruby_jr.forward(20)
+def newrow():
+    timmy.penup()
+    timmy.setpos(0, y)
+    timmy.pendown()
 
-ruby_jr = t.Turtle()
-ruby_jr.shape("turtle")
-
+timmy = t.Turtle()
+timmy.color("white")
+timmy.shape("turtle")
+my_screen = t.Screen()
+my_screen.bgcolor(113,98,122)
 
 while cont:
-    for i in range(0,5):
+    while count <5:
         dotrow()
-    turnleft()
-    for i in range(0,5):
-        dotrow()
-    turnright()
-cont = False
-my_screen = t.Screen()
+        count+=1
+        y+= 10
+    newrow()
+    #this helps to finish the program.
+    count = 0
+    rows += 1
+    if rows == 5:  #once five rows are made the program while loop stops.
+        timmy.hideturtle()
+        cont = False
+
+
 my_screen.exitonclick()
