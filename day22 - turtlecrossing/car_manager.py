32ef2ec9fef45085cf0e800331d93fa84c7e7a3b
Ruby Rain <nhhughes10@gmail.com>
2024-09-05 18:44:12 -0400

    lane changes
    almost working

--- day22 - turtlecrossing/car_manager.py
+++ day22 - turtlecrossing/car_manager.py
@@ -6,8 +6,17 @@ COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 speed_count = 0
+FIRST_XCORD = 200
+X_CORD = 300
 
-STARTING_LANES = [()]
+#7 lanes
+FIRST_POSTIONS = [(FIRST_XCORD,-215), (FIRST_XCORD,-145), (FIRST_XCORD,-75),
+                  (FIRST_XCORD,-5), (FIRST_XCORD,65), (FIRST_XCORD,135),
+                  (FIRST_XCORD,205)]
+
+STARTING_LANES = [(X_CORD,-215), (X_CORD,-145), (X_CORD,-75),
+                  (X_CORD,-5), (X_CORD,65), (X_CORD,135),
+                  (X_CORD,205)]
 
 class CarManager(Player):
 
@@ -22,7 +31,6 @@ class CarManager(Player):
     def addcars(self):  #create a singular body
         global speed_count
         rand_color = random.choice(COLORS)
-        rand_y = random.randint(-250, 250)
         car = turtle.Turtle("square")
         car2 = turtle.Turtle("square")
         car.penup()
@@ -33,13 +41,15 @@ class CarManager(Player):
         car2.setheading(180)
         if speed_count <= 10:
             car.speed("fastest")
-            car.goto(200, rand_y)
-            car2.goto(220, rand_y)
+            first_lane = random.choice(FIRST_POSTIONS)
+            car.goto(first_lane) #spawn in lanes
+            car2.goto(first_lane)
             speed_count += 1
         else:
             car.speed("fast")
-            car.goto(300, rand_y)
-            car2.goto(320, rand_y)
+            second_lane = random.choice(STARTING_LANES)
+            car.goto(second_lane)
+            car2.goto(second_lane)
         self.car_list.append(car)
         self.car_list.append(car2)
         self.car_instance = self.car_list[0]
