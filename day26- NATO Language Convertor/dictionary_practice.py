be3dd7348184c039335dd889230f4aa2090e6772
Ruby Rain <nhhughes10@gmail.com>
2025-01-02 11:14:18 -0500

    nato conversion project
    this is the nato conversion project using list comprehension over pandas data frames and normal lists

--- day26- NATO Language Convertor/dictionary_practice.py
+++ day26- NATO Language Convertor/dictionary_practice.py
@@ -12,3 +12,5 @@ student_scores = {student:random.randint(1,100) for student in names}
 
 passed_students = {student:score for (student,score) in student_scores.items() if score > 50}
 print(passed_students)
+
+
