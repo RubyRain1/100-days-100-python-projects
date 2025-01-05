8eb92567847d4fe20a9198aec494fb68933b42ff
Holden Hughes <100702336+RubyRain1@users.noreply.github.com>
2025-01-05 13:12:23 -0500

    basic gui
    this is just messing around with basic gui

--- day 27- GUI/GUI.py
+++ day 27- GUI/GUI.py
@@ -1,16 +1,37 @@
-import tkinter
+from tkinter import *
 
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title("this is the title")
 window.minsize(height= 500, width= 300)
 
 
 
-
-my_label = tkinter.Label(text="this is a label")
+#label
+my_label = Label(text="this is a label")
 my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+#button
+
+def clicked():
+    new_text = input.get()
+    my_label.config(text= new_text)
+
+
+
+button = Button(text="this is a button", command=clicked)
+button.pack()
+
+#entry
+
+input = Entry(width= 20)
+input.pack()
+
+
+
 window.mainloop()
 
 
