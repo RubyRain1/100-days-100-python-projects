b6e01204947557a2c7f8d691a7d8cc64cde294ac
Holden Hughes <100702336+RubyRain1@users.noreply.github.com>
2025-01-05 17:45:03 -0500

    timer set up
    this started the timer set up as well as the ui set up

--- day28 - pomodoro technique/main.py
+++ day28 - pomodoro technique/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,21 +13,38 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_countdown():
+    countdown(5 * 60)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def countdown(count):
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text = f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000,countdown,count-1 )
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro Studying")
 window.config(padx=100, pady=50, bg= YELLOW)
-canvas = Canvas(width=200, height=224)
+
+title_label = Label(text= "TIMER", fg = GREEN, bg = YELLOW, font= (FONT_NAME, 50))
+
+start_button = Button(text= "Start", command=start_countdown)
+reset_button = Button(text= "Reset")
+check_label = Label(text= "✔", fg= GREEN, bg= YELLOW, font= (FONT_NAME, 20, "bold"))
+canvas = Canvas(width=200, height=224, background= YELLOW, highlightthickness= 0)
 tomato_image = PhotoImage(file="tomato.png")
-canvas.create_image(103,112, image = tomato_image)
-canvas.create_text(103,112, text="00:00", fill = "white", font= (FONT_NAME, 35, "bold"))
+canvas.create_image(100,112, image = tomato_image)
+timer_text = canvas.create_text(100,130, text="00:00", fill = "white",
+                                font= (FONT_NAME, 35, "bold"))
 
-canvas.pack()
-
+title_label.grid(column = 1, row= 0)
+canvas.grid(column = 1, row = 1)
+start_button.grid(column = 0 , row = 2)
+reset_button.grid(column = 2, row = 2)
+check_label.grid(column = 1, row = 3)
 
 
 window.mainloop()
