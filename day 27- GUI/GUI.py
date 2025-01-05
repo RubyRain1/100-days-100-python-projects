23a1f81034ec6028c61a9d05906c2a97987a7edf
Holden Hughes <100702336+RubyRain1@users.noreply.github.com>
2025-01-05 13:34:50 -0500

    some more GUI
    This is just utilizing different positions

--- day 27- GUI/GUI.py
+++ day 27- GUI/GUI.py
@@ -1,34 +1,31 @@
 from tkinter import *
 
-
-window = Tk()
-
-window.title("this is the title")
-window.minsize(height= 500, width= 300)
-
-
-
-#label
-my_label = Label(text="this is a label")
-my_label.pack()
-
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
-#button
-
 def clicked():
     new_text = input.get()
     my_label.config(text= new_text)
 
+window = Tk()
+window.title("this is the title")
+window.minsize(height= 300, width= 500)
+window.config(padx= 20, pady= 20)
 
+
+#label
+my_label = Label(text="this is a label")
+my_label["text"] = "New Text"
+my_label.config(text="New Text", pady=50, padx= 50)
+my_label.grid(column = 0, row = 0)
+
+#button
 
 button = Button(text="this is a button", command=clicked)
-button.pack()
-
+button2 = Button(text="new button")
+button.grid(column = 1, row = 1)
+button2.grid(column = 2, row = 0)
 #entry
 
 input = Entry(width= 20)
-input.pack()
+input.grid(column = 3, row = 2)
 
 
 
