17beb38ccd3cb31293f8d19f136d59447430a34b
Holden Hughes <100702336+RubyRain1@users.noreply.github.com>
2025-01-27 10:32:17 -0500

    chagnes
    I cant remember what was added and what was changed, almost finished with the project tho :3

--- day31- FlashCards/Flash_Cards.py
+++ day31- FlashCards/Flash_Cards.py
@@ -17,32 +17,55 @@ data_dict = dict(zip(data['Japanese'], data['English']))
 language_list = [key for key in data_dict.keys()]
 word_list = [value for value in data_dict.values()]
 i = 0
+is_flipped = False
+def changecard():
+    global i, is_flipped
+    if not is_flipped:
+        canvas.itemconfig(language_label, text="Japanese")
+        canvas.itemconfig(word_label, text=language_list[i])
+        canvas.itemconfig( canvas_image, image=card_image)
+    else:
+        canvas.itemconfig(canvas_image, image=back_image)
+        canvas.itemconfig(language_label, text="English")
+        canvas.itemconfig(word_label, text=word_list[i])
+    is_flipped = not is_flipped
 
+    # Schedule the next flip in 3000 ms
+    window.after(3000, changecard)
+
+# CHECK IF CAN DO THIS WITH ITTERROWS!!!
 def forward_items():
     global i
-    canvas.itemconfig(language_label, text= language_list[i])
-    canvas.itemconfig(word_label, text = word_list[i])
+
+    canvas.itemconfig(word_label, text = language_list[i +1])
     i+=1
+
+
     print(i)
+
 def backward_items():
     global i
-
-    canvas.itemconfig(language_label, text=language_list[i])
-    canvas.itemconfig(word_label, text=word_list[i])
-    i-=1
-
+    if i < 0:
+        canvas.itemconfig(word_label, text= language_list[0])
+        switch = 0
+    elif i>0:
+        canvas.itemconfig(word_label, text=language_list[i - 1])
+        i-=1
+        switch = 0
     print(i)
+
 #-----------------UI SET UP-----------#
 #images
 check_image = PhotoImage(file="images/right.png")
 wrong_image = PhotoImage(file="images/wrong.png")
 card_image = PhotoImage(file="images/card_front.png")
+back_image = PhotoImage(file="images/card_back.png")
 
 #canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness= 0)
-canvas.create_image(400,263,image = card_image)
-language_label = canvas.create_text(400,150, text="language", font= ("Ariel,",40,"italic"))
-word_label =canvas.create_text(400,263, text= "word", font= ("Ariel,",60,"bold"))
+canvas_image =canvas.create_image(400,263,image = card_image)
+language_label = canvas.create_text(400,150, text="Japanese", font= ("Ariel,",40,"italic"))
+word_label =canvas.create_text(400,263, text= language_list[0], font= ("Ariel,",60,"bold"))
 #buttons
 check_button = Button(image= check_image, highlightthickness= 0, command= forward_items)
 wrong_button = Button(image= wrong_image, highlightthickness= 0, command= backward_items)
@@ -53,4 +76,6 @@ check_button.grid(column = 0, row = 1)
 wrong_button.grid(column = 1, row = 1)
 
 
+
+changecard()
 window.mainloop()
